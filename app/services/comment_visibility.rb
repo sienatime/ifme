665b6834e164ed428441a69d1901853ac9a116d8
Siena Aguayo <siena.aguayo@gmail.com>
2016-12-23 20:44:54 -0800

    added CommentVisibility class

---
+++ app/services/comment_visibility.rb
@@ -0,0 +1,51 @@
+class CommentVisibility
+  attr_reader :comment, :owner, :current_user
+
+  def initialize(comment, commented_on, current_user)
+    @comment = comment
+    @owner = User.find(commented_on.userid)
+    @current_user = current_user
+  end
+
+  def build
+    if @comment.visibility == 'private' && logged_in_user_can_view_comment?
+      I18n.t('shared.comments.visible_only_between_you_and',
+             name: other_person.name)
+    end
+  end
+
+  private
+
+  def other_person
+    if logged_in_as_owner?
+      if (viewer = User.where(id: @comment.viewers[0]).first)
+        # you are logged in as owner, you made the comment,
+        # and it is visible to a viewer
+        viewer
+      else
+        # you are logged in as owner, and comment was made by somebody else
+        User.find(@comment.comment_by)
+      end
+    else
+      # you are logged in as comment maker, and it is visible to you and owner
+      @owner
+    end
+  end
+
+  def logged_in_as_owner?
+    @owner.id == @current_user.id
+  end
+
+  def logged_in_user_made_comment?
+    @comment.comment_by == @current_user.id
+  end
+
+  def logged_in_user_is_viewer?
+    !@comment.viewers.blank? && @comment.viewers.include?(@current_user.id)
+  end
+
+  def logged_in_user_can_view_comment?
+    logged_in_user_made_comment? || logged_in_as_owner? ||
+      logged_in_user_is_viewer?
+  end
+end
